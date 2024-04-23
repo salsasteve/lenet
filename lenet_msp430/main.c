ef0505cd3f90436f158fa89ed0a734fffb5a3f7b
Salsa Steve <elmanstevelaguna@gmail.com>
2024-04-22 22:29:30 -0500

    Latest msp430 lenet

--- lenet_msp430/main.c
+++ lenet_msp430/main.c
@@ -1,26 +1,58 @@
+/*
+ * main.c
+ * Include processing inputs, outputs and applying deep learning models
+ */
+
 #include "main.h"
-#include <stdint.h>
+// static uint16_t i = 0, k = 0, j = 0, m = 0;
 
+#pragma PERSISTENT(LAYER_0)
+static layer_t LAYER_0 = {.class=Conv2d, .activation=&fp_tanh, .kernel=&CONV2D_1_WEIGHTS_MAT, .bias=&CONV2D_1_BIASES_MAT,
+                                                    .numChannels=1, .numFilters=8, .padding=Same, .stride_numCols=1, .stride_numRows=1};
 
+// #pragma PERSISTENT(LAYER_1)
+// static layer_t LAYER_1 = {.class=Maxpooling, .activation=&fp_linear, .numChannels=8, .padding=Valid, .stride_numCols=3, .stride_numRows=3, .pool_numCols=3, .pool_numRows=3};
 
-int16_t main(void)
-{
-    int16_t result = fp_exp(8, 10, 8);
+// #pragma PERSISTENT(LAYER_2)
+// static layer_t LAYER_2 = {.class=Conv2d, .activation=&fp_relu, .kernel=&MNIST_KERNEL_1_MAT, .bias=&MNIST_BIAS_1_MAT,
+//                                                     .numChannels=8, .numFilters=16, .padding=Same, .stride_numCols=1, .stride_numRows=1};
 
-    matrix inputMatrix = { .numRows = 3, .numCols = 1 };
-    matrix resultMatrix = { .numRows = 3, .numCols = 1 };
-    
-    dtype inputData[3] = {195, -62, 449};  // Fixed-point representation of [0.76, -0.24, 1.76]
-    dtype resultData[3] = {0};
-    
-    inputMatrix.data = inputData;
-    resultMatrix.data = resultData;
+// #pragma PERSISTENT(LAYER_3)
+// static layer_t LAYER_3 = {.class=Maxpooling, .activation=&fp_linear, .numChannels=16, .padding=Valid, .stride_numCols=3, .stride_numRows=3, .pool_numCols=3, .pool_numRows=3};
 
-    softmax(&resultMatrix, &inputMatrix, 8);
-    int16_t x = int_to_fp(1, 8);
-    x = fp_tanh(x, 8);
+// #pragma PERSISTENT(LAYER_4)
+// static layer_t LAYER_4 = {.class=Flatten, .numChannels=16};
 
-    return x;
+// #pragma PERSISTENT(LAYER_5)
+// static layer_t LAYER_5 = {.class=Dense, .activation=&fp_relu, .kernel=&MNIST_KERNEL_2_MAT, .bias=&MNIST_BIAS_2_MAT};
+
+// #pragma PERSISTENT(LAYER_6)
+// static layer_t LAYER_6 = {.class=Dense, .activation=&fp_relu, .kernel=&MNIST_KERNEL_3_MAT, .bias=&MNIST_BIAS_3_MAT};
+
+// #pragma PERSISTENT(LAYERS)
+// static layer_t *LAYERS[7] = {&LAYER_0, &LAYER_1, &LAYER_2, &LAYER_3, &LAYER_4, &LAYER_5, &LAYER_6};
+
+#pragma PERSISTENT(LAYERS)
+static layer_t *LAYERS[7] = {&LAYER_0};
+
+#pragma PERSISTENT(MODEL)
+static model_t MODEL = {.numLayers=1, .layers=LAYERS, .input=&MNIST_INPUT_MAT, .output=&MNIST_OUTPUT_MAT};
+
+void mnist_test(){
+   predict(&MODEL);
 }
 
+void main(void){
 
+    /* stop watchdog timer */
+    WDTCTL = WDTPW | WDTHOLD;
+
+    /* initialize GPIO System */
+    init_gpio();
+
+    /* initialize the clock and baudrate */
+    init_clock_system();
+
+    // evaluate(128,256);
+    mnist_test();
+}
--- lenet_msp430/main.h
+++ lenet_msp430/main.h
@@ -1,9 +1,17 @@
-#include "msp430.h"
+#include <layers/predict.h>
+#include <msp430.h>
+
 #include "DSPLib.h"
+#include "init.h"
+#include "neural_network_parameters.h"
 #include "math/matrix_ops.h"
 #include "math/fixed_point_ops.h"
 #include "math/matrix.h"
-#include "neural_network_parameters.h"
 #include "utils/utils.h"
-#include "init.h"
+#include "layers/layers.h"
+#include "layers/trainable.h"
 
+#ifndef MAIN_GUARD
+#define MAIN_GUARD
+
+#endif
